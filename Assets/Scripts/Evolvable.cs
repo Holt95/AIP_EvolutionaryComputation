777af0bcdb62ce6a66ad45e019ffa7b73c51e9d2
Holt95 <patrick1995holt@gmail.com>
2017-05-30 15:32:07 +0200

    Project start

---
+++ Assets/Scripts/Evolvable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Evolvable : MonoBehaviour {
+
+    public Genome genome;
+
+    public virtual float GetScore()
+    {
+        return 0;
+    }
+}
