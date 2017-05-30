7d7aa042b240d02f4afc6449f8e7d12b9e841e64
Fred <Generalissues@hotmail.com>
2017-05-30 20:33:09 +0200

    comments

--- Assets/Scripts/ControllerSpring.cs
+++ Assets/Scripts/ControllerSpring.cs
@@ -29,14 +29,25 @@ public class ControllerSpring : MonoBehaviour
 
     // Update is called once per frame
     void FixedUpdate () {
-        spring.distance = linearInterpolation(-1, +1, contracted, relaxed, position);
+        spring.distance = linearInterpolation(-1, 1, contracted, relaxed, position);
     }
 
-    public static float linearInterpolation(float x0, float x1, float y0, float y1, float x)
+    /// <summary>
+    /// Maps values from range of -1 to 1 over to the spring joint values of contracted and relaxed
+    /// </summary>
+    /// <param name="x0">-1</param>
+    /// <param name="x1">1</param>
+    /// <param name="y0">contracted</param>
+    /// <param name="y1">relaxed</param>
+    /// <param name="pos">position</param>
+    /// <returns></returns>
+    public static float linearInterpolation(float x0, float x1, float y0, float y1, float pos)
     {
         float d = x1 - x0;
         if (d == 0)
+        {
             return (y0 + y1) / 2;
-        return y0 + (x - x0) * (y1 - y0) / d;
+        }
+        return y0 + (pos - x0) * (y1 - y0) / d;
     }
 }
